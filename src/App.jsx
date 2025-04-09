ee8f120f50b2aec41ca9c40d24912de1424a243c
gardoffwhite <gardoffwhite@gmail.com>
2025-04-09 16:43:02 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Gacha Game</h1>
+      {/* UI Component หรือ logic ที่จะทำการแสดงผล */}
+    </div>
+  );
+}
+
+export default App;
