06fcac84530bf68aa8c7e95b461899b52ea21dc8
gardoffwhite <gardoffwhite@gmail.com>
2025-04-09 18:19:41 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/public/index.html',  // ทำให้เส้นทาง URL ถูกต้อง
+  base: '/src/App.jsx',  // ทำให้เส้นทาง URL ถูกต้อง
 });
