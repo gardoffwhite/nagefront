946d84c6d873a05a8f41539264e635a69adaf52c
gardoffwhite <gardoffwhite@gmail.com>
2025-04-09 18:37:30 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-// App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Gacha from './pages/Gacha';
 import AdminDashboard from './pages/AdminDashboard';
-import './style.css';  // นำเข้าไฟล์ CSS
+import Navbar from './components/Navbar'; // Import navbar
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/gacha" element={<Gacha />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-      </div>
+      <Navbar /> {/* แสดง navbar ในทุกๆ หน้า */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/gacha" element={<Gacha />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
     </Router>
   );
 }
