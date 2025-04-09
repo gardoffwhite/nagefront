86ed68a75ba92176d9df741e3d3ad25822ae2532
gardoffwhite <gardoffwhite@gmail.com>
2025-04-09 17:24:55 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav style={{ marginBottom: '20px', borderBottom: '1px solid #ccc', paddingBottom: '10px' }}>
       <Link to="/" style={{ marginRight: '10px' }}>Login</Link>
-      <Link to="/tregister" style={{ marginRight: '10px' }}>Register</Link>
+      <Link to="/register" style={{ marginRight: '10px' }}>Register</Link>
       <Link to="/gacha" style={{ marginRight: '10px' }}>Gacha</Link>
       <Link to="/admin" style={{ marginRight: '10px' }}>Admin Dashboard</Link>
     </nav>
