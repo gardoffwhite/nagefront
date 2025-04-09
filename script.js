0b952f922680baf5f30ab05766a51d390b57de11
gardoffwhite <gardoffwhite@gmail.com>
2025-04-09 15:36:14 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('signupForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    // Logic for signing up
+});
+
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    // Logic for logging in
+});
+
+document.getElementById('randomItemBtn').addEventListener('click
