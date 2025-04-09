3aa5f1680c7558b7ee5a537b9bceaa0a0d9faf14
gardoffwhite <gardoffwhite@gmail.com>
2025-04-09 18:38:12 +0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
-// index.jsx
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // ใช้สำหรับ React 18
-import App from './App';  // นำเข้า App component
-import './style.css';  // นำเข้าไฟล์ CSS ที่ใช้สำหรับการจัดแต่ง UI
+import ReactDOM from 'react-dom/client';
+import App from './App'; // อย่าลืม import คอมโพเนนต์ App
 
-// เรนเดอร์แอปลงใน root div
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
