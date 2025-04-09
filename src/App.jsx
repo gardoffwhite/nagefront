5e815bf04b514b396b29bac583b5be044054c400
gardoffwhite <gardoffwhite@gmail.com>
2025-04-09 17:20:05 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Tregister from './pages/register';
+import register from './pages/register';
 import Gacha from './pages/Gacha';
 import AdminDashboard from './pages/AdminDashboard';
 
@@ -10,7 +10,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/tregister" element={<register />} />
+        <Route path="/register" element={<register />} />
         <Route path="/gacha" element={<Gacha />} />
         <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
